1b4a1056df1ba10c9bcab4d179c929844a180b94
calvince <calokongo@gmail.com>
2022-08-25 22:42:17 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code your solution here
+// // code your solution here
+
+
+// function superBowlWin["win"] {
+//     return (win % 3 === 1);
+//   }
+// const superbowlWin = [
+//     { year: "2015", result:"win"},
+//     { year: "2014", result:"win"},
+//     { year: "2013", result:"win"},
+//     //...
+//   ]
+
+// [2015].find(superbowlWin); 
+// [2014].find(superbowlWin); 
+// [2013].find(superbowlWin); 
+
+
+
+const superbowlWin = (record) => {
+    const result = record.find(record => record.result === "W")
+
+    if (result) {
+        return result.year
+    } else {
+        return undefined
+    }
+}
